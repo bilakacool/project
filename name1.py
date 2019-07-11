d7b2d8b7eb15fb516432fa82f47b5c8b2b52f215
chenmo <837652421@qq.com>
2019-07-11 11:41:45 +0800

    Initial commit

---
+++ name1.py
@@ -0,0 +1,40 @@
+
+
+
+# test_id = 0
+# user_name = 'admin'
+# user_pass = '123456'
+#
+# while test_id == 0:
+#     for test_id2 in range(3):
+#         input_name = input('请输入用户名：')
+#         if input_name == user_name:
+#             test_id = 2
+#             while test_id == 2:
+#                 for test_id3 in range(3):
+#                     input_pass = input('请输入密码：')
+#                     if input_pass == user_pass:
+#                         print('登录成功')
+#                         test_id = 1
+#                     else:
+#                         print('密码错误，请重新输入')
+#                 print('密码三次输入错误，请退出')
+#                 break
+#
+#         else:
+#             print('用户名输入错误，请重新输入')
+#     print('用户三次输入错误，请退出')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
